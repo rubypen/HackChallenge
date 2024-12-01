bea5fd5410de3ef0a23c56dfaf33b974027a6bfb
Samantha Ahn <sa2389@cornell.edu>
2024-11-30 22:16:11 -0500

    Initial Commit

---
+++ HackChallenge2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackChallenge2024
+//
+//  Created by Samantha Ahn on 11/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
