cff498283afdd45bdcedddc15d5b1ae8aa3f518e
Ruby <rsp227@cornell.edu>
2024-12-02 16:23:54 -0500

    Update Class Cell Aesthetics

--- HackChallenge2024/Views/ClassCellView.swift
+++ HackChallenge2024/Views/ClassCellView.swift
@@ -11,17 +11,20 @@ struct ClassCellView: View {
     let classItem: ClassItem
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
+        VStack(alignment: .center, spacing: 8) {
             Text(classItem.name)
                 .font(.headline)
                 .foregroundColor(.black)
+                .multilineTextAlignment(.center)
             Text(classItem.schedule)
                 .font(.subheadline)
                 .foregroundColor(.white.opacity(0.8))
+                .multilineTextAlignment(.center)
         }
         .padding()
-        .background(Color.gray.opacity(0.9))
-        .cornerRadius(15)
-        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
+        .frame(width: 300, height: 120)
+        .background(Color.blue.opacity(0.3))
+        .cornerRadius(30)
+        .shadow(color: Color.black.opacity(0.5), radius: 5, x: 0, y: 5)
     }
 }
